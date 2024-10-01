2ff012dd828e308c316e4c8a48fca36299afc932
naphteine <58526116+naphteine@users.noreply.github.com>
2024-10-01 05:07:37 +0300

    Update footer header

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ export default function Home() {
   return (
     <>
       <header>
-      	<h1>Yakuza Virtual Gathering</h1>
+        <h1>Yakuza - Virtual Gathering Space</h1>
       </header>
 
-      <main>
-      	
-      </main>
+      <main></main>
 
-      <footer>2024 &copy; <Link href="https://gokay.works">Gökay Gültekin</Link></footer>
+      <footer>
+        2024 &copy; <Link href="https://gokay.works">Gökay Gültekin</Link>
+        {" | "}
+        <Link href="https://github.com/naphteine/yakuza">Source code</Link>
+      </footer>
     </>
   );
 }
